b1fc8847e5a8403c5db201252710e0f6af4d349b
mnapo <napolimatiase@gmail.com>
2025-04-07 09:27:40 -0300

    Added dot product method and some error messages, and changed e to E to highlight it

--- src/cls/Unit.lua
+++ src/cls/Unit.lua
@@ -1,14 +1,16 @@
 local ClassPrototype = require("ClassPrototype")
 local Unit = {}
 
-local e = math.exp(1)
+local ERROR_NOT_R_VECTOR = "Error, it can only be calculated the dot product between two real numbers vectors"
+local ERROR_DIFFERENT_DIMENSIONS = "The dot product between two vectors can't be calculated if they're of different dimensions"
+local E = math.exp(1)
 
 local sigmoid = function(z)
-    return 1/(1+e^((-1)*z))
+    return 1/(1+E^((-1)*z))
 end
 
 local tanh = function(z)
-    return (e^z-e^((-1)*z))/(e^z+e^((-1)*z))
+    return (E^z-E^((-1)*z))/(E^z+E^((-1)*z))
 end
 
 local relu = function(z)
@@ -17,6 +19,22 @@ local relu = function(z)
     else
         return 0
     end
+end
+
+local calculate_dot_product = function(v1, v2)
+    if not (v1:is_R_Vector() and v2:is_R_Vector()) then
+        error(ERROR_NOT_R_VECTOR)
+    end
+    local v1_dimension = v1:get_dimension()
+    if not (v1_dimension == v2:get_dimension()) then
+        error(ERROR_DIFFERENT_DIMENSIONS)
+    end
+    local dot_product = 0
+    for i = 1, v1_dimension do
+        local coordinates_product = v1:get_value(i) * v2:get_value(i)
+        dot_product = dot_product + coordinates_product
+    end
+    return dot_product
 end
 
 function Unit.new(network_id)
