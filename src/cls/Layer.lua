4e6da18e75bbbbc6d7ffc943cdecfe414ba4c789
mnapo <napolimatiase@gmail.com>
2025-04-11 12:08:39 -0300

    initialize classes Layer and NeuralNetwork

--- src/cls/Layer.lua
+++ src/cls/Layer.lua
@@ -1,21 +1,61 @@
 local ClassPrototype = require("ClassPrototype")
-local Unit = require("Unit")
+local R_Vector = require("R_Vector")
+local Layer = ClassPrototype:new()
+Layer.__index = Layer
 
-function Layer:new(units, activation_function)
+local ERROR_NEXT_LAYER_UNDEFINED = "There isn't a next layer defined"
+local ERROR_NOT_UNIT = "Error, Unit expected. Got "
+
+function Layer:new(network, units, activation_function, type)
     local instance = ClassPrototype:new()
 
-    activation_function = activation_function or ""
-    units = units or {}
+    local activation_function = activation_function or ""
+    local units = units or {}
+    local type = type or "hidden"
 
     instance:set("activation_function", activation_function)
-    :set("layers", layers)
+    :set("is_Unit", true)
+    :set("network", network)
+    :set("type", type)
+    :set("units", units)
 
     instance = setmetatable(instance, self)
     instance.__index = self
     return instance
 end
 
-function Layer:add_unit(unit)
+function Layer:push(u)
+    if not (is_Unit(u)) then
+        return false, ERROR_NOT_UNIT..type(u)
+    end
+    local units = self:get("units")
+    units[#units+1] = u
+    return self
+end
+
+function Layer:pop()
+    local units = self:get("units")
+    units[#units] = nil
+    return self
+end
+
+function Layer:get_output(input)
+    local units = self:get("units")
+    if self:get("type") == "output" then
+        local unit = units[#units]
+        return unit:get_output(input)
+    elseif (self:get("network"):get_layer(self:get("id")+1)) then
+        local network = self:get("network")
+        local next_layer = network:get_layer(id)
+        local next_layer_units = next_layer:get("units")
+        local values = {}
+        for i = 1, #units do
+            values[i] = units[i]:get_output(next_layer_units[i])
+        end
+        return R_Vector:new(values)
+    else
+        return ERROR_NEXT_LAYER_UNDEFINED
+    end
 end
 
 return Layer
--- src/cls/NeuralNetwork.lua
+++ src/cls/NeuralNetwork.lua
@@ -1,13 +1,26 @@
 local ClassPrototype = require("ClassPrototype")
 local Layer = require("Layer")
+local NeuralNetwork = ClassPrototype:new()
+NeuralNetwork.__index = NeuralNetwork
 
-function NeuralNetwork:new(layers, activation_function)
+local ERROR_NOT_LAYER = "Error, Layer expected. Got "
+local ERROR_NOT_NUMBER = "Id number needs to be a positive integer"
+
+local is_Layer = function(x)
+    if not (type(x) == "table") then
+        return false
+    end
+    return x["is_Layer"] or false
+end
+
+function NeuralNetwork:new(layers, type, activation_function)
     local instance = ClassPrototype:new()
 
-    activation_function = activation_function or ""
-    layers = layers or {}
+    local activation_function = activation_function or ""
+    local layers = layers or {}
 
     instance:set("activation_function", activation_function)
+    :set("last", 0)
     :set("layers", layers)
 
     instance = setmetatable(instance, self)
@@ -15,7 +28,44 @@ function NeuralNetwork:new(layers, activation_function)
     return instance
 end
 
-function NeuralNetwork:add_layer(name, units)
+function NeuralNetwork:push(l)
+    if not (is_Layer(l)) then
+        return false, ERROR_NOT_LAYER..type(l)
+    end
+    local new_id = self:get("last")+1
+    local layers = self:get("layers")
+    layers[#layers+1] = l
+    l:set(new_id)
+    return self
+end
+
+function NeuralNetwork:pop()
+    local layers = self:get("layers")
+    layers[#layers] = nil
+    return self
+end
+
+function NeuralNetwork:get_layer(id)
+    if (type(id) ~= "number" or id <= 0) then
+        error(ERROR_NOT_NUMBER)
+    end
+    if id > self:get("last") then
+        return false
+    end
+    return true
+end
+
+function NeuralNetwork:get_output(input)
+    local layers = self:get("layers")
+    local output = 0
+    for i = 1, #layers do
+        local layer = self:get_layer(i)
+        input = layer:calculate_output(input)
+        if layer:get("type") == "output" then
+            output = input
+        end
+    end
+    return output
 end
 
 return NeuralNetwork
