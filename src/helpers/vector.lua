aebc430619b3f87ab6fc3f22fa00ec5bc54e13a6
mnapo <napolimatiase@gmail.com>
2025-04-11 08:13:24 -0300

    add tostring function

--- src/helpers/vector.lua
+++ src/helpers/vector.lua
@@ -1,5 +1,9 @@
+local r_vector = require("R_Vector")
 local vector = {}
 
+local BRACKET_END = ")"
+local BRACKET_START = "("
+local DELIMITER = ";"
 local ERROR_NOT_R_VECTOR = "Error, it can only be calculated the dot product/vectorial sum between two real numbers vectors"
 local ERROR_DIFFERENT_DIMENSIONS = "The dot product between two vectors can't be calculated if they're of different dimensions"
 local ERROR_CANT_OPERATE_BETWEEN = "Error, you can't operate between these objects"
@@ -35,11 +39,27 @@ end
 vector.vectorial_sum = function(v1, v2)
     local can_operate, err = vector.can_operate_between(v1, v2)
     if not can_operate then error(err) end
-    local sum = 0
+    local values = {}
     for i = 1, v1:get_dimension() do
-        sum = sum + v1:get_value(i) + v2:get_value(i)
+       values[i] = v1:get_value(i) + v2:get_value(i)
     end
-    return sum
+    return r_vector:new(values)
+end
+
+vector.tostring = function(v)
+    local can_operate, err = vector.can_operate_between(v, v)
+    if not can_operate then error(err) end
+    local dimension = v:get_dimension()
+    local vector_as_string = BRACKET_START
+    for i = 1, dimension do
+        vector_as_string = vector_as_string..v:get_value(i)
+        if i < dimension then
+            vector_as_string = vector_as_string..DELIMITER
+        else
+            vector_as_string = vector_as_string..BRACKET_END
+        end
+    end
+    return vector_as_string
 end
 
 return vector
