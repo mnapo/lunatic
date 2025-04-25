0406a4b51b9684612e9e45f21efb2101288f6118
mnapo <napolimatiase@gmail.com>
2025-04-25 15:19:03 -0300

    add print method

--- src/cls/Vocabulary.lua
+++ src/cls/Vocabulary.lua
@@ -2,20 +2,32 @@ local ClassPrototype = require("ClassPrototype")
 local Vocabulary = ClassPrototype:new()
 Vocabulary.__index = Vocabulary
 
+local VOCABULARY_DEFAULT_NAME = "Unnamed"
 local GRANULARITY_LEVELS = {"words","subwords"}
 
-function Vocabulary:new(tokens, granularity)
+function Vocabulary:new(tokens, granularity, name)
     local instance = ClassPrototype:new()
 
     local granularity = granularity or GRANULARITY_LEVELS[1]
+    local name = name or VOCABULARY_DEFAULT_NAME
     local tokens = tokens or {}
 
     instance:set("granularity", granularity)
+    :set("name", name)
     :set("tokens", tokens)
 
     instance = setmetatable(instance, self)
     instance.__index = self
     return instance
+end
+
+function Vocabulary:print()
+    local tokens = self:get("tokens")
+    print(self:get("name").." Vocabulary")
+    print("\tGranularity level: "..self:get("granularity"))
+    for i = 1, #tokens do
+        print("\tToken #"..i..": "..tokens[i])
+    end
 end
 
 function Vocabulary:push(t)
