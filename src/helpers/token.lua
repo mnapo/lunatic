37adbd5644dff9dc80017efe6cc908ab0f886bd1
mnapo <napolimatiase@gmail.com>
2025-04-25 15:20:29 -0300

    change induce function

--- src/helpers/token.lua
+++ src/helpers/token.lua
@@ -1,8 +1,18 @@
 local M = {}
 
-M.to_words = function(source)
-end
+M.CAPTURE_PATTERN_START = "([^"
+M.CAPTURE_PATTERN_END = "]+)"
+M.SEPARATOR = "%s"
+M.gmatch = string.gmatch
+M.VOCABULARIES_COUNT = 0
 
+M.to_words = function(source)
+    local temp = {}
+    for word in M.gmatch(source, M.CAPTURE_PATTERN_START..M.SEPARATOR..M.CAPTURE_PATTERN_END) do
+        temp[#temp+1] = word
+    end
+    return temp
+end
 M.to_subwords = function(source)
 end
 
@@ -11,15 +21,16 @@ M.TOKENIZATION_METHODS = {
     to_subwords = M.to_subwords
 }
 
-M.induce = function(source, method)
+M.induce = function(source, method, name)
     local granularity_level = method
     if method == "words" then
         method = M.TOKENIZATION_METHODS.to_words
     elseif method == "subwords" then
         method = M.TOKENIZATION_METHODS.to_subwords
     end
-    local induced = Vocabulary:new(method(source), granularity_level)
-    return induced
+    M.VOCABULARIES_COUNT = M.VOCABULARIES_COUNT + 1
+    local name = name or "#"..M.VOCABULARIES_COUNT
+    return method(source), granularity_level, name
 end
 
 M.segment = function(vocabulary, test, method)
