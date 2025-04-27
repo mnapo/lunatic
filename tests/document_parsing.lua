4047ebde7bb11051b756382fce1ea0399e50248f
mnapo <napolimatiase@gmail.com>
2025-04-27 18:29:47 -0300

    add tests with newer to_words function

--- tests/document_parsing.lua
+++ tests/document_parsing.lua
@@ -2,15 +2,21 @@ local token = require("token")
 local vocabulary = require("Vocabulary")
 
 local M = {}
-M.SAMPLE_DOCUMENT_CONTENT = "This is a short sample document"
-M.SAMPLE_DOCUMENT_NAME = "Test"
+M.SAMPLE_DOCUMENT_1_CONTENT = "This is a short sample document"
+M.SAMPLE_DOCUMENT_2_CONTENT = "And another sample and this time without repetition this time"
 
 M.run = function()
-    local source = M.SAMPLE_DOCUMENT
-    local tokens, granularity, name = token.induce(M.SAMPLE_DOCUMENT_CONTENT, "words", M.SAMPLE_DOCUMENT_NAME)
-    local v = vocabulary:new(tokens, granularity, name)
-    print("Source Document: "..M.SAMPLE_DOCUMENT_CONTENT)
-    v:print()
+    local source = M.SAMPLE_DOCUMENT_1_CONTENT
+    local tokens, granularity, name = token.induce(source, "words", "Test 1")
+    local v1 = vocabulary:new(tokens, granularity, name)
+    print("Source Document: "..source)
+    v1:print()
+
+    source = M.SAMPLE_DOCUMENT_2_CONTENT
+    tokens, granularity, name = token.induce(source, "words", "Test 2")
+    local v2 = vocabulary:new(tokens, granularity, name)
+    print("Source Document: "..source)
+    v2:print()
 end
 
 return M
