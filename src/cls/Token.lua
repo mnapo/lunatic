f3edf19dce39e1f56635b659dc060002baf874b6
mnapo <napolimatiase@gmail.com>
2025-05-03 04:41:36 -0300

    add Token class

---
+++ src/cls/Token.lua
@@ -0,0 +1,24 @@
+local Token = ClassPrototype:new()
+Token.__index = Token
+
+function Token:new(morpheme)
+    local instance = ClassPrototype:new()
+
+    local morpheme = morpheme or ""
+
+    instance:set("morpheme", morpheme)
+
+    instance = setmetatable(instance, self)
+    instance.__index = self
+    return instance
+end
+
+function Token:set_morpheme(new_value)
+    self:set("morpheme", new_value)
+end
+
+function Token:print()
+    print(self:get("morpheme"))
+end
+
+return Token
