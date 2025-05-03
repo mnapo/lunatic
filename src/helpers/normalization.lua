36c881c3cb7cfd698bdd63628370850275365a14
mnapo <napolimatiase@gmail.com>
2025-05-03 18:38:38 -0300

    declare normalization helper functions

---
+++ src/helpers/normalization.lua
@@ -0,0 +1,42 @@
+local M = {}
+
+M.get_min_edit_distance = function(token1, token2)
+end
+
+M.expand = function(contraction)
+end
+
+M.get_affixes = function(token)
+end
+
+M.get_stem = function(token)
+end
+
+M.porter_stemming = function(token)
+end
+
+M.lemmatize = function(token)
+end
+
+M.parse_morphologically = function(token)
+end
+
+M.case_folding = function(token)
+end
+
+M.unify = function(tokens)
+end
+
+M.named_entity_recognition = function(tokens)
+end
+
+M.sentence_segmentation = function(source)
+end
+
+M.sentences_as_tokens = function(source)
+end
+
+M.split_by_lines = function(source)
+end
+
+return M
