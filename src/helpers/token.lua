689fd6d8c3711785b71a4c0c3f971795d5bea17a
mnapo <napolimatiase@gmail.com>
2025-05-03 02:21:31 -0300

    add subword tokenization functions

--- src/helpers/token.lua
+++ src/helpers/token.lua
@@ -2,15 +2,54 @@ local M = {}
 
 M.CAPTURE_PATTERN_START = "([^"
 M.CAPTURE_PATTERN_END = "]+)"
+M.ERROR_INSUFFICIENT_TOKENS = "There's not enough tokens to sort (there should be two at least)"
 M.ERROR_METHOD = "Invalid method"
+M.ERROR_TO_BE_REPORTED = 'Internal Lua "error" or really strange behaviour is happening. We are going to see what is going on next time'
 M.MAX_SUBWORDS_LEARNING = 10
+M.MIN_TOKENS = 2
+M.MORPHEME_KEY_ID = "char-"
+M.TOKENS_THRESHOLD = 4
+M.TRACING_SPACE = "_"
+M.TRACING_SPACE_DOUBLE = "__"
 M.VOCABULARIES_COUNT = 0
 M.WHITE_SPACE = "%s"
 M.gmatch = string.gmatch
 M.len = string.len
 M.lower = string.lower
+M.sub = string.sub
 
-M.tokenize = function(source, delimiter)
+M.tokenize_by_pairs = function(tokens)
+    local temp = M.tokenize_by_characters(tokens)
+end
+
+M.tokenize_by_characters = function(tokens)
+    local temp = {}
+    for i = 1, #tokens do
+        local morpheme = tokens[i].morpheme
+        local length = M.len(morpheme)
+        for j = 1, length do
+            local character = M.sub(morpheme, j, j)
+            local key = M.MORPHEME_KEY_ID..character
+            if temp[key] == nil then
+                temp[key] = 1
+            else
+                local total = temp[key]+1
+                temp[key] = total
+            end
+        end
+    end
+    for morpheme, frequency in pairs(temp) do
+        if tonumber(morpheme) then
+            print(M.ERROR_TO_BE_REPORTED)
+        else
+            temp[#temp+1] = {morpheme=morpheme, frequency=frequency}
+            temp[morpheme] = nil
+        end
+    end
+    return temp
+end
+
+M.tokenize_by_delimiter = function(source, delimiter)
     local temp = {}
     for morpheme in M.gmatch(source, M.CAPTURE_PATTERN_START..delimiter..M.CAPTURE_PATTERN_END) do
         morpheme = M.lower(morpheme)
@@ -27,25 +66,53 @@ M.tokenize = function(source, delimiter)
     return temp
 end
 
-M.to_words = function(source, delimiter)
-    return M.tokenize(source, M.WHITE_SPACE)
+M.to_words = function(source, add_tracing_space)
+    local temp = M.tokenize_by_delimiter(source, M.WHITE_SPACE)
+    if add_tracing_space then
+        for i = 1, #temp do
+            if (M.len(temp[i].morpheme)%2==0) then
+                temp[i].morpheme = temp[i].morpheme..M.TRACING_SPACE_DOUBLE
+            else
+                temp[i].morpheme = temp[i].morpheme..M.TRACING_SPACE
+            end
+        end
+    end
+    return temp
+end
+
+M.sort_by_frequency = function(tokens, descending)
+    if #tokens<M.MIN_TOKENS then
+        return error(M.ERROR_INSUFFICIENT_TOKENS)
+    end
+    local compare_frequencies = function(token_1, token_2)
+        if descending then
+            return token_1.frequency > token_2.frequency
+        else
+            return token_1.frequency < token_2.frequency
+        end
+    end
+    table.sort(tokens, compare_frequencies)
+end
+
+M.trim_less_frequent = function(tokens)
+    for i = M.TOKENS_THRESHOLD+1, #tokens do
+        tokens[i] = nil
+    end
+end
+
+M.split_with_tracing_space = function(source)
+    local temp = M.to_words(source, true)
 end
 
 M.bytepair_encoding = function(source)
-    local vocabulary = M.tokenize(source, M.WHITE_SPACE)
-    local length = M.len(source)
-    if length%2 ~= 0 then
-        source = source.." "
-    end
-    for i = 1, length, 2 do
-        print(string.sub(source, i, i+1))
-    end
-    return {}
+    local tokens = M.to_words(source, true)
+    tokens = M.tokenize_by_characters(tokens)
+    return tokens
 end
 
 M.to_subwords = function(source)
-    return M.bytepair_encoding(source)
-    --return M.bytepair_encoding(source, M.MAX_SUBWORDS_LEARNING)
+    local vocabulary = M.bytepair_encoding(source)
+    return vocabulary
 end
 
 M.TOKENIZATION_METHODS = {
--- tests/document_parsing.lua
+++ tests/document_parsing.lua
@@ -2,9 +2,8 @@ local token = require("token")
 local vocabulary = require("Vocabulary")
 
 local M = {}
-M.SAMPLE_DOCUMENT_1_CONTENT = "This is a short sample document"
+M.SAMPLE_DOCUMENT_1_CONTENT = "This is a short sample document document sample short"
 M.SAMPLE_DOCUMENT_2_CONTENT = "And another sample and this time without repetition this time"
-M.SAMPLE_DOCUMENT_3_CONTENT = "low lowest newer wider new"
 
 M.run = function()
     local source = M.SAMPLE_DOCUMENT_1_CONTENT
@@ -13,17 +12,11 @@ M.run = function()
     print("Source Document: "..source)
     v1:print()
 
-    source = M.SAMPLE_DOCUMENT_2_CONTENT
-    tokens, granularity, name = token.induce(source, "words")
+    source = M.SAMPLE_DOCUMENT_1_CONTENT
+    tokens, granularity, name = token.induce(source, "subwords")
     local v2 = vocabulary:new(tokens, granularity, name)
     print("Source Document: "..source)
     v2:print()
-
-    source = M.SAMPLE_DOCUMENT_3_CONTENT
-    tokens, granularity, name = token.induce(source, "subwords")
-    local v3 = vocabulary:new(tokens, granularity, name)
-    print("Source Document: "..source)
-    v3:print()
 end
 
 return M
