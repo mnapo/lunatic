04c4015d33791b5fe34fc91b7f7ced1ce41fa9b0
mnapo <napolimatiase@gmail.com>
2026-07-01 18:27:37 -0300

    export math modules

--- lunatic/math/init.lua
+++ lunatic/math/init.lua
@@ -2,4 +2,8 @@ return {
     Vector = require("lunatic.math.vector"),
     Matrix = require("lunatic.math.matrix"),
     Tensor = require("lunatic.math.tensor"),
+    constants = require("lunatic.math.constants"),
+    linalg = require("lunatic.math.linalg"),
+    statistics = require("lunatic.math.statistics"),
+    random = require("lunatic.math.random"),
 }
