ca19cce82359edfc7fe74cb29b2f74db9fba9abc
mnapo <napolimatiase@gmail.com>
2026-07-07 09:33:45 -0300

    create basic autograd's Node implementation

---
+++ lunatic/math/autograd/node.lua
@@ -0,0 +1,35 @@
+local Node = {}
+Node.__index = Node
+
+function Node.new(operation, inputs, backward_fn)
+
+    assert(type(operation) == "string",
+        "Node.new(): operation must be string")
+
+    assert(type(inputs) == "table",
+        "Node.new(): inputs must be table")
+
+    assert(type(backward_fn) == "function",
+        "Node.new(): backward_fn must be function")
+
+    local self = setmetatable({}, Node)
+
+    self.operation = operation
+    self.inputs = inputs
+    self.backward_fn = backward_fn
+
+    return self
+end
+
+
+function Node:backward(gradient)
+
+    return self.backward_fn(
+        gradient,
+        self.inputs
+    )
+
+end
+
+
+return Node
