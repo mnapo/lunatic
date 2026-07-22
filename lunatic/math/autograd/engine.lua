b094f1796e129091f5a398958af46c5dfbaad885
mnapo <napolimatiase@gmail.com>
2026-07-22 20:01:04 -0300

    implement depth first search

--- lunatic/math/autograd/engine.lua
+++ lunatic/math/autograd/engine.lua
@@ -2,6 +2,30 @@ local Context = require("lunatic.math.autograd.context")
 
 local Engine = {}
 
+
+local function build_graph(tensor, visited, nodes)
+
+    if tensor.grad_fn == nil then
+        return
+    end
+
+    local node = tensor.grad_fn
+
+    if visited[node] then
+        return
+    end
+
+    visited[node] = true
+
+    for _, input in ipairs(node.inputs) do
+        build_graph(input, visited, nodes)
+    end
+
+    table.insert(nodes, node)
+
+end
+
+
 function Engine.backward(tensor, gradient)
 
     assert(
@@ -9,26 +33,50 @@ function Engine.backward(tensor, gradient)
         "Tensor does not have a computation graph"
     )
 
-    local node = tensor.grad_fn
+    local nodes = {}
+
+    build_graph(
+        tensor,
+        {},
+        nodes
+    )
+
+
+    tensor.grad = gradient
+
 
     Context.disable()
 
-    local gradients = node:backward(gradient)
 
-    Context.enable()
+    for i = #nodes, 1, -1 do
 
-    for i, input in ipairs(node.inputs) do
+        local node = nodes[i]
 
-        local grad = gradients[i]
+        local output_grad = node.output.grad
 
-        if input.grad == nil then
-            input.grad = grad
-        else
-            input.grad = input.grad + grad
+        local gradients = node:backward(
+            output_grad
+        )
+
+
+        for j, input in ipairs(node.inputs) do
+
+            local grad = gradients[j]
+
+            if input.grad == nil then
+                input.grad = grad
+            else
+                input.grad = input.grad + grad
+            end
+
         end
 
     end
 
+
+    Context.enable()
+
 end
+
 
 return Engine
