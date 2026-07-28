be2858d3ebc5e9fb33ab07b9e4c3a95486de4dc9
mnapo <napolimatiase@gmail.com>
2026-07-28 18:13:50 -0300

    integrate backwards broadcasting to mul operation

--- lunatic/math/autograd/functions/mul.lua
+++ lunatic/math/autograd/functions/mul.lua
@@ -1,15 +1,25 @@
+local broadcast = require("lunatic.math.broadcast")
+
 local Mul = {}
 
-function Mul.backward(gradient, inputs, output)
+function Mul.backward(gradient, inputs)
 
     local a = inputs[1]
     local b = inputs[2]
 
-    return {
-        gradient * b,
-        gradient * a
-    }
+    local grad_a = gradient:mul(b)
+    local grad_b = gradient:mul(a)
 
+    return {
+        broadcast.reduce_gradient(
+            grad_a,
+            a.shape
+        ),
+        broadcast.reduce_gradient(
+            grad_b,
+            b.shape
+        )
+    }
 end
 
 return Mul
